ecce898bf6a704909f9112a16e68886f3caef4f7
kot <stas@jresearch.org>
2018-02-07 20:58:31 +0100

    Update dep.

--- backend/src/main/java/conf/BackendConfig.java
+++ backend/src/main/java/conf/BackendConfig.java
@@ -2,8 +2,6 @@ package conf;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ComponentScan("org.jresearch.gavka.web")
