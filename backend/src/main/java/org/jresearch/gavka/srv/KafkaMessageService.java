cc38681a8a165cb5329a0354e382568ae46ce524
cathy <cathy@jresearch.org>
2019-02-03 11:04:55 +0100

    cluster service

--- backend/src/main/java/org/jresearch/gavka/srv/KafkaMessageService.java
+++ backend/src/main/java/org/jresearch/gavka/srv/KafkaMessageService.java
@@ -10,9 +10,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Properties;
-import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
