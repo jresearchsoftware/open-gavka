42607934f7eef0877a35607c711a6d28f96afcbd
kot <stas@jresearch.org>
2019-03-16 13:23:57 +0100

    Add generated icons
    --HG--
    branch : Connections

--- gwt.core/src/main/java/org/jresearch/gavka/gwt/core/client/module/connection/editor/MdIconSelect.java
+++ gwt.core/src/main/java/org/jresearch/gavka/gwt/core/client/module/connection/editor/MdIconSelect.java
@@ -1,23 +1,33 @@
 package org.jresearch.gavka.gwt.core.client.module.connection.editor;
 
-import java.util.EnumSet;
+import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
+
+import javax.annotation.Nonnull;
 
 import org.dominokit.domino.ui.forms.Select;
 import org.dominokit.domino.ui.forms.SelectOption;
 import org.dominokit.domino.ui.icons.MdiIcon;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableList.Builder;
+
 public class MdIconSelect extends Select<String> {
 
 	public MdIconSelect(String label) {
-		super(label, toSelectOprions(EnumSet.allOf(MdIconEnum.class)));
+		super(label, toSelectOprions(MdIcons.VALUES));
 	}
 
-	private static List<SelectOption<String>> toSelectOprions(EnumSet<MdIconEnum> colors) {
-		return colors.stream().map(MdIconEnum::getIcon).map(MdIconSelect::toSelectOption).collect(Collectors.toList());
+	private static List<SelectOption<String>> toSelectOprions(Collection<MdIcons> icons) {
+		Builder<SelectOption<String>> builder = ImmutableList.<SelectOption<String>>builder();
+		for (MdIcons icon : icons) {
+			builder.add(toSelectOption(icon.icon()));
+		}
+		return builder.build();
+//		return icons.stream().map(MdIcons::getIcon).map(MdIconSelect::toSelectOption).collect(Collectors.toList());
 	}
 
+	@Nonnull
 	private static SelectOption<String> toSelectOption(MdiIcon icon) {
 		SelectOption<String> option = SelectOption.create(icon.getName(), icon.getName());
 		option.getValueContainer().appendChild(icon);
