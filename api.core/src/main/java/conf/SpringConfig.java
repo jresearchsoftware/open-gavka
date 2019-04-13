6d3877d21eedb83282b0a13185c04f500b318e76
kot <stas@jresearch.org>
2019-04-14 00:05:26 +0200

    GAV-22 Create new consumer view for topic

--- api.core/src/main/java/conf/SpringConfig.java
+++ api.core/src/main/java/conf/SpringConfig.java
@@ -1,9 +1,18 @@
 package conf;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.server.adapter.ForwardedHeaderTransformer;
 
 @Configuration
 @EnableScheduling
+@SuppressWarnings("static-method")
 public class SpringConfig {
+
+	@Bean
+	public ForwardedHeaderTransformer forwardedHeaderTransformer() {
+		return new ForwardedHeaderTransformer();
+	}
+
 }
--- api.core/src/main/java/conf/WebSocketServerConfiguration.java
+++ api.core/src/main/java/conf/WebSocketServerConfiguration.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
-import org.springframework.web.server.adapter.ForwardedHeaderTransformer;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
@@ -29,11 +28,6 @@ public class WebSocketServerConfiguration implements WebSocketConfigurer {
 	@Bean
 	public TaskScheduler taskScheduler() {
 		return new ConcurrentTaskScheduler(Executors.newSingleThreadScheduledExecutor());
-	}
-
-	@Bean
-	public ForwardedHeaderTransformer forwardedHeaderTransformer() {
-		return new ForwardedHeaderTransformer();
 	}
 
 }
