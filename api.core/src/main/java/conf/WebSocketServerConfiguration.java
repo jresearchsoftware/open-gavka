a9d9efe39894fa87fb0d80ac9039d31897b638e3
kot <stas@jresearch.org>
2019-04-13 21:25:41 +0200

    GAV-22 Create new consumer view for topic

--- api.core/src/main/java/conf/WebSocketServerConfiguration.java
+++ api.core/src/main/java/conf/WebSocketServerConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
+import org.springframework.web.server.adapter.ForwardedHeaderTransformer;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
@@ -29,4 +30,10 @@ public class WebSocketServerConfiguration implements WebSocketConfigurer {
 	public TaskScheduler taskScheduler() {
 		return new ConcurrentTaskScheduler(Executors.newSingleThreadScheduledExecutor());
 	}
+
+	@Bean
+	public ForwardedHeaderTransformer forwardedHeaderTransformer() {
+		return new ForwardedHeaderTransformer();
+	}
+
 }
